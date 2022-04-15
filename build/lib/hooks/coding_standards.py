95d1133496aec1dbf4d0c7c84eb9ab66fb6cf4d8
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-04-15 07:19:30 +0000

    updates

---
+++ build/lib/hooks/coding_standards.py
@@ -0,0 +1,100 @@
+# !/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+# maintainer (@pandanz)
+"""
+Ensure coding standards are used.
+"""
+
+import difflib
+from pathlib import Path
+import sys
+import pkgutil
+
+DOCUMENTATION = """
+Ensures that files related to coding standards are consistent with pre-defined templates.
+If there is difference between these files, the files are updated with the repo.
+"""
+
+RETURNS = """
+PASS/FAIL
+Lists files that were modified or created.
+"""
+
+
+def diff_file(filename, content):
+    """Diff a file."""
+
+    # full_path = Path(__file__).parent.joinpath(content)
+    # print(full_path)
+    return list(
+        difflib.context_diff(
+            [f"{line}\n" for line in Path(filename).read_text().split("\n")],
+            [f"{line}\n" for line in content.split("\n")],
+            # [f"{line}\n" for line in Path(content).read_text().split("\n")],
+            filename,
+            "generated",
+        )
+    )
+
+
+def _coding_standards():
+    """
+    Inital entrypoint from __main__:
+    Prints: files that were modified or created
+    Returns: bool
+    """
+    error = False
+    files = (
+        (".gitignore", "templates/gitignore_template"),
+        (".flake8", "templates/flake8_template"),
+        (".editorconfig", "templates/editorconfig_template"),
+        (".gitattributes", "templates/gitattributes_template"),
+        ("pyproject.toml", "templates/pyproject_template"),
+        (".vscode/settings.json", "templates/vscode_settings_template"),
+    )
+
+    differences = []
+    files_modified = []
+    # print(f"Working Directory {Path().resolve()}")
+
+    for filename, template in files:
+        file = Path(filename)
+        content = pkgutil.get_data("hooks", template).decode()
+        file.parent.mkdir(exist_ok=True, parents=True)
+        file.touch(exist_ok=True)
+        diff = diff_file(filename, content)
+        if diff:
+            differences.append("".join(diff))
+            files_modified.append(filename)
+            print("ERROR - FOUND THE FOLLOWING DIFFERENCES")
+            print()
+            print()
+            print("\n\n".join(differences))
+            print()
+            Path(filename).write_text(content)
+
+    if files_modified:
+        for file in files_modified:
+            print(f"{file} was modified, defaulting back to standard")
+        error = True
+
+    return error
+
+
+def main():
+    """
+    Checks local repo files are consistent with templates
+    Returns: bool as sys.exit code.  True = 1, False = 0.  Zero is good.
+    """
+    error = False
+
+    error = _coding_standards()
+
+    if error is True:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,39 @@
+"""
+Sets up pre-commit
+"""
+from datetime import datetime
+from setuptools import setup, find_packages
+
+PACKAGES = find_packages(exclude=["test*", "tests*", "testing*"])
+
+PROJECT_NAME = "pre-commit-coding-standards"
+PROJECT_PACKAGE_NAME = "pre-commit-coding-standards"
+PROJECT_LICENSE = "MIT"
+PROJECT_AUTHOR = "@johnsondnz"
+PROJECT_COPYRIGHT = f"2018-{datetime.now().year}"
+PROJECT_URL = "https://github.com/johnsondnz/pre-commit-ansible-secrets"
+PROJECT_EMAIL = "donaldjohnsond.nz@gmail.com"
+PROJECT_VERSION = "1.0.3"
+
+REQUIRES = [
+    "flake8==3.9.2",
+    "six",
+    "typing",
+    "PyYAML>=5.1.2",
+    "black==21.9b0",
+    "GitPython==3.1.24",
+]
+
+setup(
+    name=PROJECT_PACKAGE_NAME,
+    version=PROJECT_VERSION,
+    url=PROJECT_URL,
+    author=PROJECT_AUTHOR,
+    author_email=PROJECT_EMAIL,
+    packages=PACKAGES,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIRES,
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["coding-standards = hooks.coding_standards:main"]},
+)
